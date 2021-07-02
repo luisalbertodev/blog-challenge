28fc5c18647f6b1687f3304cd7b24e0ea724e57e
Luis Alberto Pérez Coello <luisalbertodev@gmail.com>
2021-07-02 02:23:40 -0500

    credentials add to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCFi6Y92eXOFxaBFGGhDX-q5reOwoQAT1c',
+    authDomain: 'blog-challenge-18f9b.firebaseapp.com',
+    projectId: 'blog-challenge-18f9b',
+    storageBucket: 'blog-challenge-18f9b.appspot.com',
+    messagingSenderId: '963060237099',
+    appId: '1:963060237099:web:46d301906f3255ebf7bb6e',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCFi6Y92eXOFxaBFGGhDX-q5reOwoQAT1c',
+    authDomain: 'blog-challenge-18f9b.firebaseapp.com',
+    projectId: 'blog-challenge-18f9b',
+    storageBucket: 'blog-challenge-18f9b.appspot.com',
+    messagingSenderId: '963060237099',
+    appId: '1:963060237099:web:46d301906f3255ebf7bb6e',
+  },
 };
 
 /*
