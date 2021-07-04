8238d53b3505663764d985e4535186e1ba0ee2bf
Luis Alberto Pérez Coello <luisalbertodev@gmail.com>
2021-07-04 11:07:07 -0500

    added modules

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { AuthService } from './services/auth/auth.service';
+import { PostsService } from './services/posts/posts.service';
+import { SEOService } from './services/SEOService/seo.service';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  providers: [AuthService, PostsService, SEOService],
 })
-export class CoreModule { }
+export class CoreModule {}
