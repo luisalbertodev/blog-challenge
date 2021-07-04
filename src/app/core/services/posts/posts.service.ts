6cd6c06c7c406c23fa693ec948d66970d4d2c0dc
Luis Alberto Pérez Coello <luisalbertodev@gmail.com>
2021-07-04 10:50:39 -0500

    added service post

--- src/app/core/services/posts/posts.service.ts
+++ src/app/core/services/posts/posts.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+import { Post, FetchService } from '@app/types/post.model';
+import { environment } from 'src/environments/environment';
+
+// apiKeyServiceNews
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostsService {
+  posts: Post[];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllPosts(country: string, category: string) {
+    return this.http.get<FetchService>(
+      `${environment.baseUrl}&category=${category}&country=${country}`
+    );
+  }
 }
