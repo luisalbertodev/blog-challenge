677016dcdede8954c3ad3e365511e8a83abbb639
Luis Alberto Pérez Coello <luisalbertodev@gmail.com>
2021-07-04 11:11:26 -0500

    was added lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,50 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutComponent } from '@app/layout/layout.component';
+import { sectionsMetadata } from '@app/shared/constants/sectionsMetadata';
+import { AppGuard } from '@app/guard/app.guard';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    canActivate: [AppGuard],
+    children: [
+      {
+        path: '',
+        redirectTo: '/inicio?categoria=business&pais=MX',
+        pathMatch: 'full',
+      },
+      {
+        path: 'inicio',
+        data: sectionsMetadata.homePage,
+        loadChildren: () =>
+          import('@app/home/home.module').then((m) => m.HomeModule),
+      },
+    ],
+  },
+  {
+    path: 'seguridad',
+    loadChildren: () =>
+      import('@app/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: '**',
+    data: sectionsMetadata.notFound,
+    loadChildren: () =>
+      import('@app/page-not-found/page-not-found.module').then(
+        (m) => m.PageNotFoundModule
+      ),
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules,
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
