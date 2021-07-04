a4237e4b57fcfa8993328644d8aa75c7c9390083
Luis Alberto Pérez Coello <luisalbertodev@gmail.com>
2021-07-04 11:09:30 -0500

    added logic for guard

--- src/app/guard/app.guard.ts
+++ src/app/guard/app.guard.ts
@@ -1,15 +1,38 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
 import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { AuthService } from '@app/core/services/auth/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.authService.hasUser().pipe(
+      map((hasUser) => {
+        return hasUser !== null;
+      }),
+      tap((hasUser) => {
+        if (!hasUser) {
+          this.router.navigate(['/seguridad/identificate']);
+        }
+      })
+    );
   }
-  
 }
