d03cf55b78e15f77598ff8a857ab61a65346934b
Luis Alberto Pérez Coello <luisalbertodev@gmail.com>
2021-07-04 10:56:06 -0500

    added modules

--- src/app/post/post.module.ts
+++ src/app/post/post.module.ts
@@ -1,21 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
-import { PostRoutingModule } from './post-routing.module';
 import { PostComponent } from './components/post/post.component';
 import { PostsComponent } from './components/posts/posts.component';
 import { PostDetailComponent } from './components/post-detail/post-detail.component';
-
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
-  declarations: [
-    PostComponent,
-    PostsComponent,
-    PostDetailComponent
-  ],
-  imports: [
-    CommonModule,
-    PostRoutingModule
-  ]
+  declarations: [PostComponent, PostsComponent, PostDetailComponent, NotFoundComponent],
+  exports: [PostComponent, PostsComponent, PostDetailComponent],
+  imports: [CommonModule, RouterModule],
 })
-export class PostModule { }
+export class PostModule {}
