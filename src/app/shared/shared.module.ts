87322a62a8578fad49a50d8c9615a2c8592e57bd
Luis Alberto Pérez Coello <luisalbertodev@gmail.com>
2021-07-04 11:05:51 -0500

    add modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/header/header.component';
+import { RouterModule } from '@angular/router';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FooterComponent } from './components/footer/footer.component';
-
-
+import { SelectComponent } from './components/select/select.component';
+import { MenuDesktopComponent } from './components/header/components/menu-desktop/menu-desktop.component';
+import { MenuMobileComponent } from './components/header/components/menu-mobile/menu-mobile.component';
+import { HeaderComponent } from './components/header/components/header/header.component';
+import { AvatarComponent } from './components/avatar/avatar.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SelectComponent,
+    MenuDesktopComponent,
+    MenuMobileComponent,
+    AvatarComponent,
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    SelectComponent,
+    MenuDesktopComponent,
+    MenuMobileComponent,
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ScrollingModule,
+    BrowserAnimationsModule,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
