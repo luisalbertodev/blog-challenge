3f0a695b1a2c25538b3c4f4a61f05fb636896f5d
Luis Alberto Pérez Coello <luisalbertodev@gmail.com>
2021-07-04 11:11:40 -0500

    added modules and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,29 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+import { ToastrModule } from 'ngx-toastr';
+
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { environment } from '@env/environment';
-import { LayoutComponent } from './layout/layout.component';
+import { LayoutComponent } from '@app/layout/layout.component';
+import { SharedModule } from '@app/shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent, LayoutComponent],
   imports: [
+    FormsModule,
+    SharedModule,
     BrowserModule,
+
+    HttpClientModule,
     AppRoutingModule,
+    ToastrModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
   ],
