a58c0e8c8047704eb9c34793ce6dfa65b095b1ad
Luis Alberto Pérez Coello <luisalbertodev@gmail.com>
2021-07-04 10:58:41 -0500

    added metadata for SEO

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SEOService } from '@src/app/core/services/SEOService/seo.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor(private SEOService: SEOService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    const { meta } = this.route.snapshot.data;
+    this.SEOService.setTagsAndTitleToPage(meta);
   }
-
 }
