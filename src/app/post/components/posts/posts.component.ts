cc5118ccf2646f79cfbe26cec8f2c1538dafecc0
Luis Alberto Pérez Coello <luisalbertodev@gmail.com>
2021-07-04 10:45:45 -0500

    was completed component posts

--- src/app/post/components/posts/posts.component.ts
+++ src/app/post/components/posts/posts.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { PostsService } from '@app/core/services/posts/posts.service';
+import { Post } from '@app/types/post.model';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+  styleUrls: ['./posts.component.scss'],
 })
 export class PostsComponent implements OnInit {
+  posts: Post[] = [];
 
-  constructor() { }
+  constructor(
+    private postsService: PostsService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.fetchProducts();
   }
 
+  fetchProducts() {
+    this.route.queryParams.subscribe((params: Params) => {
+      this.postsService
+        .getAllPosts(params.pais ?? '', params.categoria ?? '')
+        .subscribe(({ articles }) => {
+          this.posts = articles;
+        });
+    });
+  }
 }
