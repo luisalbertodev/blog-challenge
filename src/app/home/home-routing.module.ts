693dba3c82863a15c58f228cc4fabb880f6d8950
Luis Alberto Pérez Coello <luisalbertodev@gmail.com>
2021-07-04 11:02:21 -0500

    added modules

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './components/home/home/home.component';
-
+import { HomeComponent } from './components/home/home.component';
+import { PostModule } from '@app/post/post.module';
+import { ChipComponent } from './components/chip/chip.component';
+import { ChipsComponent } from './components/chips/chips.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomeComponent, ChipComponent, ChipsComponent],
+  imports: [CommonModule, HomeRoutingModule, PostModule],
 })
-export class HomeModule { }
+export class HomeModule {}
