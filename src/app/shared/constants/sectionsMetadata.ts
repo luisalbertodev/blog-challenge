92308e5d0216ad855bfa6fd13b025900e50c8265
Luis Alberto Pérez Coello <luisalbertodev@gmail.com>
2021-07-04 11:05:17 -0500

    was added mock data for categories, countries and metadata

---
+++ src/app/shared/constants/sectionsMetadata.ts
@@ -0,0 +1,29 @@
+export const sectionsMetadata = {
+  homePage: {
+    meta: {
+      title: 'Publicaciones | Bambu mobile',
+      description:
+        'Conoce las ultimas noticias de más de 15 regiones diferentes',
+    },
+  },
+  register: {
+    meta: {
+      title: 'Registro | Bambu mobile',
+      description:
+        'Este proceso no tomara más de 5 minutos y nos ayudara a brindarte una mejor experiencia.',
+    },
+  },
+  login: {
+    meta: {
+      title: 'Iniciar sesión | Bambu mobile',
+      description:
+        'Identificate para acceder a ver el nuevo contenido que tenemos.',
+    },
+  },
+  notFound: {
+    meta: {
+      title: '404 | Bambu mobile',
+      description: 'Página no disponible',
+    },
+  },
+};
