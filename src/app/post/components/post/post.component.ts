7f8534ef1f3066755e9bc514673188b6219b5885
Luis Alberto Pérez Coello <luisalbertodev@gmail.com>
2021-07-04 10:45:57 -0500

    was completed component post

--- src/app/post/components/post/post.component.ts
+++ src/app/post/components/post/post.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from '@app/types/post.model';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
 })
 export class PostComponent implements OnInit {
+  @Input() post: Post;
+  views: number = Math.round(Math.random() * 1000);
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
